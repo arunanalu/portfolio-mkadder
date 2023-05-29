e20b2f87bc986ff4f47cd429631454ea73f3b801
mayu <mayusatori@gmail.com>
2023-05-29 07:49:38 -0300

    small change

--- components/Header.js
+++ components/Header.js
@@ -8,7 +8,7 @@ const Header = () => {
   return (
       <div className="select-none flex flex-col w-screen py-12 lg:py-20 h-max
       justify-center items-center gap-y-6 sm:gap-y-12 md:gap-y-14">
-        <h1 className="main-title text-2xl sm:text-4xl md:text-5xl font-bold">KADDER</h1>
+        <h1 className="main-title text-2xl sm:text-4xl md:text-5xl font-bold">Kadder</h1>
         <Navbar />
       </div>
   )
